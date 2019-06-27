6413193a47829394ee23ff1e2ab23d2316b78e35
Ko-Dell <komyo2000@gmail.com>
2019-06-27 23:14:14 +0700

    added solution of finding sqrt

---
+++ src/com/kkmhh/codingchallenge/codility/FindingSqrt.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kkmhh.codingchallenge.codility;
+
+/**
+ *
+ * @author KoKo
+ */
+public class FindingSqrt {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println(solution(1000, 2000));
+    }
+
+    public static int solution(int A, int B) {
+        // write your code in Java SE 8
+//        int sqr = B;
+
+        int res = 0;
+        while (B > 1) {
+            res = B;
+            B = (int) Math.sqrt(B);
+            System.out.println("sqr : " + B);
+        }
+        return res;
+    }
+
+}
